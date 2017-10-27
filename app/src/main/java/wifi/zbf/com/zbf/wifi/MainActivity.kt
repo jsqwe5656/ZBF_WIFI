78464ec6f38682a27d46139efc224559fa5e38a0
bf_zhang <516845590@qq.com>
2017-10-27 15:43:19 +0800

    Initial commit

---
+++ app/src/main/java/wifi/zbf/com/zbf/wifi/MainActivity.kt
@@ -0,0 +1,12 @@
+package wifi.zbf.com.zbf.wifi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
